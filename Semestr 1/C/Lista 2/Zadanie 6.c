0d7a01e2c5280bd32060c2d870b128acec720b2e
Damian-Roszak <zakladylaboratoryjneroszaka@gmail.com>
2022-06-16 20:26:01 +0200

    Create Zadanie 6.c

---
+++ Semestr 1/C/Lista 2/Zadanie 6.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+#include<math.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void main(){
+int itab[5], pom;
+
+
+
+printf ("\n Zadanie 6 \n ");
+printf ("Podaj liczbę: ");
+int lz6, lc6;
+scanf ("%d", &lz6);
+printf ("\n pierwiastek wynosi = %f", sqrt(lz6));
+lc6 = sqrt(lz6);
+if (lc6 == sqrt(lz6)) printf ("\n %d jest liczbą kwadratową", lz6);
+else printf ("\n opcja 2 lc6 = %d", lc6);
+
+
+
+printf("\n");
+}
