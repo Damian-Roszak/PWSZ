3c5ad200bafb45837b47bca258bf1eab4751936e
Damian Roszak <zakladylaboratoryjneroszaka@gmail.com>
2022-06-18 16:02:44 +0200

    Create Program.cs

---
+++ Semestr 4/NET/Lista 1/Zad 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LAB03marzec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.Write("Podaj liczbę : ");
+            x = int.Parse(Console.ReadLine());
+
+            int n=0, i;
+
+            for ( i = 1; i <= x; i++)
+            {
+                if(x%i==0) n++;
+            }
+            if (n==2)
+                Console.WriteLine("Liczba "+x+" jest liczbą pierwszą.");
+            else
+                Console.WriteLine("to nie " + x + " liczba pierwsza.");
+        }
+    }
+}
