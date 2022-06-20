cd1df56c678055d1744a7b0ab0b812edf2a20e06
Damian Roszak <zakladylaboratoryjneroszaka@gmail.com>
2022-06-20 20:59:37 +0200

    Add files via upload

---
+++ Semestr 4/NET/Lista 1/Zad3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class opmat
+{
+    static void Main()
+    {
+        int n;
+        Console.Write("Podaj liczbę: ");
+        n = int.Parse(Console.ReadLine());
+        Console.WriteLine(" ");
+        double ulamek, calk, kwadr, pierw;
+        kwadr = 8 * n + 1;
+        pierw = Math.Sqrt(kwadr);
+        calk = Math.Truncate(pierw);
+        ulamek = pierw - calk;
+        if (ulamek == 0)
+        {
+            Console.WriteLine("WYNIK: liczba="+ n + " jest trójkątna");
+        }
+        else {
+            Console.WriteLine("liczba=" + n + " NIE jest trójkątna");
+        }
+        
+        
+    }
+}
---
+++ Semestr 4/NET/Lista 1/Zad5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class opmat
+{
+    static void Main()
+    {
+
+        double value, power;
+        Console.Write("Podaj liczbę do pierwiastkowania: ");
+        value = Double.Parse(Console.ReadLine());
+        Console.WriteLine(" ");
+
+        Console.Write("Podaj stopień pierwiastka: ");
+        power = Double.Parse(Console.ReadLine());
+        Console.WriteLine(" ");
+
+        Console.WriteLine($"{value}^{1/power} = {(long)Math.Pow(value, (1 / power)):N0} (0x{(long)Math.Pow(value, (1 / power)):X})");
+
+    }
+}
