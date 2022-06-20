ac5a270cef2ed1010d358384b7ef66f9e870ebff
Damian Roszak <zakladylaboratoryjneroszaka@gmail.com>
2022-06-20 20:58:09 +0200

    Add files via upload

---
+++ Semestr 4/NET/Lista 1/zad2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+             Console.Write("Podaj palindrom: ");
+             x = Console.ReadLine();
+            int czek = 0;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == x[x.Length - i - 1]) continue;
+                else {
+                    Console.WriteLine("wyraz 1: " + x + " to nie palindrom");
+                    czek++;
+                    break;
+                }
+            }
+            if (czek==0) Console.WriteLine("SUKCES wyraz 1: "+x+ " to palindrom");
+            
+        }
+    }
+}
---
+++ Semestr 4/NET/Lista 1/zad4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b,c,x1,x2;
+            Console.WriteLine("Podaj a=");
+            a = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj b=");
+            b = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj c=");
+            c = Double.Parse(Console.ReadLine());
+
+            if (a == 0) Console.WriteLine("To nie jest funkcja kwadratowa!!");
+            else { 
+                x1 = (b*b-4*a*c)/ 2 * a;
+                x2 = (b * b + 4 * a * c) / 2 * a;
+
+               // Console.WriteLine("Wynik: x1=" + x1 + "  x2=" + x2);
+                Console.WriteLine(String.Format("WYNIK x1={0:0.##}  x2={1:0.##}", x1,x2));
+            }
+
+        }
+    }
+}
