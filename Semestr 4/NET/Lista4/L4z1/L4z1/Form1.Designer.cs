22106efe87b587d7c5c4484f59001eb900b790ef
Damian Roszak <zakladylaboratoryjneroszaka@gmail.com>
2022-06-23 18:50:13 +0200

    Add files via upload

---
+++ Semestr 4/NET/Lista4/L4z1/L4z1/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace L4z1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.kto = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bNowaGra = new System.Windows.Forms.Button();
+            this.CZvsCZ = new System.Windows.Forms.RadioButton();
+            this.CZvsCPU = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b1.Location = new System.Drawing.Point(12, 39);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(150, 150);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b2.Location = new System.Drawing.Point(168, 39);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(150, 150);
+            this.b2.TabIndex = 1;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b3.Location = new System.Drawing.Point(324, 39);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(150, 150);
+            this.b3.TabIndex = 2;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b4.Location = new System.Drawing.Point(12, 195);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(150, 150);
+            this.b4.TabIndex = 3;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b5.Location = new System.Drawing.Point(168, 195);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(150, 150);
+            this.b5.TabIndex = 4;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b6.Location = new System.Drawing.Point(324, 195);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(150, 150);
+            this.b6.TabIndex = 5;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b7.Location = new System.Drawing.Point(12, 351);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(150, 150);
+            this.b7.TabIndex = 6;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b8.Location = new System.Drawing.Point(168, 351);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(150, 150);
+            this.b8.TabIndex = 7;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b9.Location = new System.Drawing.Point(324, 351);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(150, 150);
+            this.b9.TabIndex = 8;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(544, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 39);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Teraz Gracz:";
+            // 
+            // kto
+            // 
+            this.kto.AutoSize = true;
+            this.kto.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.kto.Location = new System.Drawing.Point(762, 107);
+            this.kto.Name = "kto";
+            this.kto.Size = new System.Drawing.Size(40, 39);
+            this.kto.TabIndex = 10;
+            this.kto.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(560, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 39);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Teraz Gracz:";
+            // 
+            // bNowaGra
+            // 
+            this.bNowaGra.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bNowaGra.Location = new System.Drawing.Point(168, 550);
+            this.bNowaGra.Name = "bNowaGra";
+            this.bNowaGra.Size = new System.Drawing.Size(150, 55);
+            this.bNowaGra.TabIndex = 12;
+            this.bNowaGra.Text = "Nowa Gra";
+            this.bNowaGra.UseVisualStyleBackColor = true;
+            this.bNowaGra.Click += new System.EventHandler(this.bNowaGra_Click);
+            // 
+            // CZvsCZ
+            // 
+            this.CZvsCZ.AutoSize = true;
+            this.CZvsCZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CZvsCZ.Location = new System.Drawing.Point(542, 12);
+            this.CZvsCZ.Name = "CZvsCZ";
+            this.CZvsCZ.Size = new System.Drawing.Size(294, 35);
+            this.CZvsCZ.TabIndex = 13;
+            this.CZvsCZ.TabStop = true;
+            this.CZvsCZ.Text = "Czlowiek vs Człowiek";
+            this.CZvsCZ.UseVisualStyleBackColor = true;
+            this.CZvsCZ.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // CZvsCPU
+            // 
+            this.CZvsCPU.AutoSize = true;
+            this.CZvsCPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CZvsCPU.Location = new System.Drawing.Point(542, 53);
+            this.CZvsCPU.Name = "CZvsCPU";
+            this.CZvsCPU.Size = new System.Drawing.Size(242, 35);
+            this.CZvsCPU.TabIndex = 14;
+            this.CZvsCPU.TabStop = true;
+            this.CZvsCPU.Text = "Czlowiek vs CPU";
+            this.CZvsCPU.UseVisualStyleBackColor = true;
+            this.CZvsCPU.CheckedChanged += new System.EventHandler(this.CZvsCPU_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(518, 243);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(402, 404);
+            this.textBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 659);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CZvsCPU);
+            this.Controls.Add(this.CZvsCZ);
+            this.Controls.Add(this.bNowaGra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.kto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label kto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bNowaGra;
+        private System.Windows.Forms.RadioButton CZvsCZ;
+        private System.Windows.Forms.RadioButton CZvsCPU;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Semestr 4/NET/Lista4/L4z1/L4z1/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L4z1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool zawodnikX = true;
+        int ruch = 0;
+        bool wygrana = false;
+        Button[] all = new Button[9];
+        int CPUwybranePole = -1;
+        bool tryb = false;
+        String winer = "nikt";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ruch = 0;
+            all[0] = b1;
+            all[1] = b2;
+            all[2] = b3;
+            all[3] = b4;
+            all[4] = b5;
+            all[5] = b6;
+            all[6] = b7;
+            all[7] = b8;
+            all[8] = b9;
+            foreach (Button b in all)
+            {
+                b.Enabled = true;
+                b.Text = "";
+            }
+        }
+
+        private void bNowaGra_Click(object sender, EventArgs e)
+        {
+            NowaGra();
+            bNowaGra.Text = "Nowa Giera";
+            kto.Text = "X";
+            zawodnikX = true;
+        }
+        public void czyjRuch()
+        {
+            if (!zawodnikX) ruchCPU();
+            //if (kto.Text == "O") ruchCPU();
+           // MessageBox.Show("kto.Text", kto.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        }
+        private void NowaGra()
+        {
+            ruch = 0;           
+            all[0] = b1;
+            all[1] = b2;
+            all[2] = b3;
+            all[3] = b4;
+            all[4] = b5;
+            all[5] = b6;
+            all[6] = b7; 
+            all[7] = b8;
+            all[8] = b9;
+            foreach(Button b in all)
+            {
+                b.Enabled = true;
+                b.Text = "";
+            }
+        }
+        private void Czek()
+        {
+            if (b1.Text != "" && b1.Text == b2.Text && b2.Text == b3.Text)
+            {
+                winer = kto.Text;
+                WinWIn();
+                return;
+            }
+            else if (b4.Text != "" && b4.Text == b5.Text && b5.Text == b6.Text)
+            {
+                winer = kto.Text;
+                WinWIn();
+                return;
+            }
+            else if (b7.Text != "" && b7.Text == b8.Text && b8.Text == b9.Text)
+            {
+                winer = kto.Text;
+                WinWIn();
+                return;
+            }
+            else if (b1.Text != "" && b1.Text == b4.Text && b4.Text == b7.Text)
+            {
+                winer = kto.Text;
+                WinWIn();
+                return;
+            }
+            else if (b2.Text != "" && b2.Text == b5.Text && b5.Text == b8.Text)
+            {
+                winer = kto.Text;
+                WinWIn();
+                return;
+            }
+            else if (b3.Text != "" && b3.Text == b6.Text && b9.Text == b6.Text)
+            {
+                winer = kto.Text;
+                WinWIn();
+                return;
+            }
+            else if (b1.Text != "" && b1.Text == b5.Text && b5.Text == b9.Text)
+            {
+                winer = kto.Text;
+                WinWIn();
+                return;
+            }
+            else if (b3.Text != "" && b3.Text == b5.Text && b5.Text == b7.Text)
+            {
+                winer = kto.Text;
+                WinWIn();
+                return;
+            }
+            else if (ruch==9)
+            {
+                MessageBox.Show("Remisik", "Remisik", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+             //czyjRuch();
+        }
+
+        private void WinWIn()
+        {
+            MessageBox.Show("Wygraniusia zawodnika: " + kto.Text, "Wygraniusia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            wygrana = true;
+            bNowaGra.PerformClick();
+            return;
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = zawodnikX ? "X" : "O";
+            ((Button)sender).Enabled = false;
+            if(ruch >= 5)
+            {
+                Czek();
+            }
+            textBox1.Text += "Zawodnik " + kto.Text + " wykonał ruch na pole nr: 1";
+            zawodnikX = !zawodnikX;
+            kto.Text = zawodnikX ? "X" : "O";
+            czyjRuch();
+        }
+
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = zawodnikX ? "X" : "O";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Czek();
+            }
+            textBox1.Text += "Zawodnik " + kto.Text + " wykonał ruch na pole nr: 2";
+            zawodnikX = !zawodnikX;
+            kto.Text = zawodnikX ? "X" : "O";
+            if(tryb) czyjRuch();
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = zawodnikX ? "X" : "O";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Czek();
+            }
+            textBox1.Text += "Zawodnik " + kto.Text + " wykonał ruch na pole nr: 3";
+            zawodnikX = !zawodnikX;
+            kto.Text = zawodnikX ? "X" : "O";
+            if (tryb) czyjRuch();
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = zawodnikX ? "X" : "O";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Czek();
+            }
+            textBox1.Text += "Zawodnik " + kto.Text + " wykonał ruch na pole nr: 4";
+            zawodnikX = !zawodnikX;
+            kto.Text = zawodnikX ? "X" : "O";
+            if (tryb) czyjRuch();
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = zawodnikX ? "X" : "O";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Czek();
+            }
+            textBox1.Text += "Zawodnik " + kto.Text + " wykonał ruch na pole nr: 5";
+            zawodnikX = !zawodnikX;
+            kto.Text = zawodnikX ? "X" : "O";
+            if (tryb) czyjRuch();
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = zawodnikX ? "X" : "O";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Czek();
+            }
+            textBox1.Text += "Zawodnik " + kto.Text + " wykonał ruch na pole nr: 6";
+            zawodnikX = !zawodnikX;
+            kto.Text = zawodnikX ? "X" : "O";
+            if (tryb) czyjRuch();
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = zawodnikX ? "X" : "O";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Czek();
+            }
+            textBox1.Text += "Zawodnik " + kto.Text + " wykonał ruch na pole nr: 7";
+            zawodnikX = !zawodnikX;
+            kto.Text = zawodnikX ? "X" : "O";
+            if (tryb) czyjRuch();
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = zawodnikX ? "X" : "O";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Czek();
+            }
+            textBox1.Text += "Zawodnik " + kto.Text + " wykonał ruch na pole nr: 8";
+            zawodnikX = !zawodnikX;
+            kto.Text = zawodnikX ? "X" : "O";
+            if (tryb) czyjRuch();
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = zawodnikX ? "X" : "O";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Czek();
+            }
+            textBox1.Text += "Zawodnik " + kto.Text + " wykonał ruch na pole nr: 9";
+            zawodnikX = !zawodnikX;
+            kto.Text = zawodnikX ? "X" : "O";
+            if (tryb) czyjRuch();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            tryb = false;
+        }
+        public void ruchCPU()
+        {
+            Random rnd = new Random();
+            String znakCPU = "O";
+            int wyznacznik = 0;
+            int czyPoleWolne = -1;
+            
+                if (kto.Text == "O")
+                {
+                    while (true)
+                    {
+                        if (all[4].Enabled == true) { all[4].PerformClick(); break; };
+                        czyPoleWolne = rnd.Next(8);
+                    czyCzWygra();
+                        if (all[czyPoleWolne].Enabled == true) { all[czyPoleWolne].PerformClick(); break; };
+                    }
+                // while(all[CPUwybranePole].Enabled == false)
+                //     CPUwybranePole = rnd.Next(8) ;
+                }
+
+           // zawodnikX = !zawodnikX;
+           // kto.Text = zawodnikX ? "X" : "O";
+
+        }
+
+        private void czyCzWygra()
+        {
+            
+        }
+
+        private async void CZvsCPU_CheckedChanged(object sender, EventArgs e)
+        {
+            tryb = true;
+            if(!zawodnikX) ruchCPU();
+            //MessageBox.Show("CZvsCPU_CheckedChanged", "CZvsCPU_CheckedChanged", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        private void czyCPUWygra()
+        {
+            if (b1.Text != "" && b1.Text == b2.Text && b2.Text == b3.Text)
+            {
+                
+            }
+            else if (b4.Text != "" && b4.Text == b5.Text && b5.Text == b6.Text)
+            {
+                
+            }
+            else if (b7.Text != "" && b7.Text == b8.Text && b8.Text == b9.Text)
+            {
+                
+            }
+            else if (b1.Text != "" && b1.Text == b4.Text && b4.Text == b7.Text)
+            {
+                
+            }
+            else if (b2.Text != "" && b2.Text == b5.Text && b5.Text == b8.Text)
+            {
+                
+            }
+            else if (b3.Text != "" && b3.Text == b6.Text && b9.Text == b6.Text)
+            {
+                
+            }
+            else if (b1.Text != "" && b1.Text == b5.Text && b5.Text == b9.Text)
+            {
+                
+            }
+            else if (b3.Text != "" && b3.Text == b5.Text && b5.Text == b7.Text)
+            {
+               
+            }
+            else if (ruch == 9)
+            {
+                MessageBox.Show("Remisik", "Remisik", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+}
