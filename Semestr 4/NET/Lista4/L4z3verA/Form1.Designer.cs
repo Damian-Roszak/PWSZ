ffe5a689398c0f7b5c6d5da40a1eca2763d09599
Damian Roszak <zakladylaboratoryjneroszaka@gmail.com>
2022-06-23 18:53:20 +0200

    Add files via upload

---
+++ Semestr 4/NET/Lista4/L4z3verA/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace L4z3verA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.locationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m1DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m2DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m3DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m4DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m5DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m6DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m7DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m8DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m9DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m10DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m11DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m12DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.temperatureBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.temperatureBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(795, 522);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.locationDataGridViewTextBoxColumn,
+            this.m1DataGridViewTextBoxColumn,
+            this.m2DataGridViewTextBoxColumn,
+            this.m3DataGridViewTextBoxColumn,
+            this.m4DataGridViewTextBoxColumn,
+            this.m5DataGridViewTextBoxColumn,
+            this.m6DataGridViewTextBoxColumn,
+            this.m7DataGridViewTextBoxColumn,
+            this.m8DataGridViewTextBoxColumn,
+            this.m9DataGridViewTextBoxColumn,
+            this.m10DataGridViewTextBoxColumn,
+            this.m11DataGridViewTextBoxColumn,
+            this.m12DataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.temperatureBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 332);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(846, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(879, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // locationDataGridViewTextBoxColumn
+            // 
+            this.locationDataGridViewTextBoxColumn.DataPropertyName = "Location";
+            this.locationDataGridViewTextBoxColumn.HeaderText = "Location";
+            this.locationDataGridViewTextBoxColumn.Name = "locationDataGridViewTextBoxColumn";
+            // 
+            // m1DataGridViewTextBoxColumn
+            // 
+            this.m1DataGridViewTextBoxColumn.DataPropertyName = "M1";
+            this.m1DataGridViewTextBoxColumn.HeaderText = "M1";
+            this.m1DataGridViewTextBoxColumn.Name = "m1DataGridViewTextBoxColumn";
+            // 
+            // m2DataGridViewTextBoxColumn
+            // 
+            this.m2DataGridViewTextBoxColumn.DataPropertyName = "M2";
+            this.m2DataGridViewTextBoxColumn.HeaderText = "M2";
+            this.m2DataGridViewTextBoxColumn.Name = "m2DataGridViewTextBoxColumn";
+            // 
+            // m3DataGridViewTextBoxColumn
+            // 
+            this.m3DataGridViewTextBoxColumn.DataPropertyName = "M3";
+            this.m3DataGridViewTextBoxColumn.HeaderText = "M3";
+            this.m3DataGridViewTextBoxColumn.Name = "m3DataGridViewTextBoxColumn";
+            // 
+            // m4DataGridViewTextBoxColumn
+            // 
+            this.m4DataGridViewTextBoxColumn.DataPropertyName = "M4";
+            this.m4DataGridViewTextBoxColumn.HeaderText = "M4";
+            this.m4DataGridViewTextBoxColumn.Name = "m4DataGridViewTextBoxColumn";
+            // 
+            // m5DataGridViewTextBoxColumn
+            // 
+            this.m5DataGridViewTextBoxColumn.DataPropertyName = "M5";
+            this.m5DataGridViewTextBoxColumn.HeaderText = "M5";
+            this.m5DataGridViewTextBoxColumn.Name = "m5DataGridViewTextBoxColumn";
+            // 
+            // m6DataGridViewTextBoxColumn
+            // 
+            this.m6DataGridViewTextBoxColumn.DataPropertyName = "M6";
+            this.m6DataGridViewTextBoxColumn.HeaderText = "M6";
+            this.m6DataGridViewTextBoxColumn.Name = "m6DataGridViewTextBoxColumn";
+            // 
+            // m7DataGridViewTextBoxColumn
+            // 
+            this.m7DataGridViewTextBoxColumn.DataPropertyName = "M7";
+            this.m7DataGridViewTextBoxColumn.HeaderText = "M7";
+            this.m7DataGridViewTextBoxColumn.Name = "m7DataGridViewTextBoxColumn";
+            // 
+            // m8DataGridViewTextBoxColumn
+            // 
+            this.m8DataGridViewTextBoxColumn.DataPropertyName = "M8";
+            this.m8DataGridViewTextBoxColumn.HeaderText = "M8";
+            this.m8DataGridViewTextBoxColumn.Name = "m8DataGridViewTextBoxColumn";
+            // 
+            // m9DataGridViewTextBoxColumn
+            // 
+            this.m9DataGridViewTextBoxColumn.DataPropertyName = "M9";
+            this.m9DataGridViewTextBoxColumn.HeaderText = "M9";
+            this.m9DataGridViewTextBoxColumn.Name = "m9DataGridViewTextBoxColumn";
+            // 
+            // m10DataGridViewTextBoxColumn
+            // 
+            this.m10DataGridViewTextBoxColumn.DataPropertyName = "M10";
+            this.m10DataGridViewTextBoxColumn.HeaderText = "M10";
+            this.m10DataGridViewTextBoxColumn.Name = "m10DataGridViewTextBoxColumn";
+            // 
+            // m11DataGridViewTextBoxColumn
+            // 
+            this.m11DataGridViewTextBoxColumn.DataPropertyName = "M11";
+            this.m11DataGridViewTextBoxColumn.HeaderText = "M11";
+            this.m11DataGridViewTextBoxColumn.Name = "m11DataGridViewTextBoxColumn";
+            // 
+            // m12DataGridViewTextBoxColumn
+            // 
+            this.m12DataGridViewTextBoxColumn.DataPropertyName = "M12";
+            this.m12DataGridViewTextBoxColumn.HeaderText = "M12";
+            this.m12DataGridViewTextBoxColumn.Name = "m12DataGridViewTextBoxColumn";
+            // 
+            // temperatureBindingSource
+            // 
+            this.temperatureBindingSource.DataSource = typeof(L4z3verA.Temperature);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 571);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Line Chart";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.temperatureBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn locationDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m1DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m2DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m3DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m4DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m5DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m6DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m7DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m8DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m9DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m10DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m11DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m12DataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource temperatureBindingSource;
+    }
+}
+
---
+++ Semestr 4/NET/Lista4/L4z3verA/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace L4z3verA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var objChart = chart1.ChartAreas[0];
+            objChart.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            objChart.AxisX.Minimum = 1;
+            objChart.AxisX.Maximum = 12;
+
+            objChart.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            objChart.AxisY.Minimum = -50;
+            objChart.AxisY.Maximum = 50;
+            chart1.Series.Clear();
+
+            Random random = new Random();
+            foreach (Temperature t in temperatureBindingSource.DataSource as List<Temperature>)
+            {
+                chart1.Series.Add(t[A][B]);
+                chart1.Series[t.Location].Color = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                chart1.Series[t.Location].Legend = "Legend1";
+                chart1.Series[t.Location].ChartArea = "ChartArea1";
+                chart1.Series[t.Location].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+                for (int i = 1; i <= 12; i++)
+                    chart1.Series[t.Location].Points.AddXY(i, Convert.ToInt32(t[$"M{i}"]));
+
+            }
+            /*   var objChart = chart1.ChartAreas[0];
+               objChart.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+               objChart.AxisX.Minimum = 1;
+               objChart.AxisX.Maximum = 12;
+
+               objChart.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+               objChart.AxisY.Minimum = -50;
+               objChart.AxisY.Maximum = 50;
+               chart1.Series.Clear();
+
+               Random random = new Random();
+               foreach (Temperature t in temperatureBindingSource.DataSource as List<Temperature>)
+               {
+                   chart1.Series.Add(t.Location);
+                   chart1.Series[t.Location].Color = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                   chart1.Series[t.Location].Legend = "Legend1";
+                   chart1.Series[t.Location].ChartArea = "ChartArea1";
+                   chart1.Series[t.Location].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+                   for (int i = 1; i <= 12; i++)
+                       chart1.Series[t.Location].Points.AddXY(i, Convert.ToInt32(t[$"M{i}"]));
+
+               }*/
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            temperatureBindingSource.DataSource= new List<Temperature>();
+        }
+    }
+}
