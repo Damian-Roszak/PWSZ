44044c15caeea6579155bd77bdb4757d98b27dd3
Damian Roszak <zakladylaboratoryjneroszaka@gmail.com>
2022-06-27 18:27:08 +0200

    Add files via upload

---
+++ Semestr 4/NET/Lista8/ConsoleApp Wątki p1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+class MyThread
+{
+	static void Main()
+	{
+		Thread watek = new Thread(JakasFunkcja);
+		watek.Start();
+		for (int i = 0; i < 10; i++) Console.Write("={0}).A=", i);
+	}
+	static void JakasFunkcja()
+	{
+		for(int i=0;i<10;i++) Console.Write("+{0}).B+",i);
+	}
+}
