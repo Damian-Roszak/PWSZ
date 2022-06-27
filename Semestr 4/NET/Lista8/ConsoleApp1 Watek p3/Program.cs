40b80626c4103f2fc6ab2073e2f07df38ebd5dda
Damian Roszak <zakladylaboratoryjneroszaka@gmail.com>
2022-06-27 18:29:15 +0200

    Add files via upload

---
+++ Semestr 4/NET/Lista8/ConsoleApp1 Watek p3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MyThread
+{
+    class Program
+    {
+        static bool zrobione;
+        static object kłódka = new object();
+        static void Main(string[] args)
+        {
+            new Thread(Uruchom).Start();
+            Uruchom();
+            Console.Read();
+        }
+
+        static void Uruchom()
+        {
+            lock (kłódka)
+            {
+                if (!zrobione)
+                {
+                    Console.WriteLine("Zrobione");
+                    zrobione = true;
+                }
+            }
+        }
+    }
+}
---
+++ Semestr 4/NET/Lista8/ConsoleApp1 Watki p2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+class WatekTestowy
+{
+    bool zrobione;
+    static void Main()
+    {
+        WatekTestowy nazwa = new WatekTestowy();
+        new Thread(nazwa.Uruchom).Start();
+        nazwa.Uruchom();
+        Console.ReadLine();
+    }
+    void Uruchom() 
+    {
+        Console.WriteLine("Piszę");
+        if (!zrobione)
+        {
+            zrobione = true;
+            Console.WriteLine("Zrobione");
+        }
+    }
+}
