2fd3b19fe59e94d57093da218924a03bd4bc9641
Damian Roszak <zakladylaboratoryjneroszaka@gmail.com>
2022-06-28 06:54:44 +0200

    Add files via upload

---
+++ Semestr 4/NET/EntityFrameworkCodeFirst/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameworkCodeFirst.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkCodeFirst.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogContext db = new BlogContext();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View(db.Blog.ToList());
+        }
+    }
+}
---
+++ Semestr 4/NET/EntityFrameworkCodeFirst/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkCodeFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Dziennik ocen stuentów.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "PWSZ.NYSA.PL";
+
+            return View();
+        }
+    }
+}
